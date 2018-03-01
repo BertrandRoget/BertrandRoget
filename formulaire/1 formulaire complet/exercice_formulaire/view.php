27d6a09f4396cacc1c84a2e8a578db241ab1188a
BertrandRoget <36259501+BertrandRoget@users.noreply.github.com>
2018-03-01 22:19:24 +0100

    Add files via upload

---
+++ formulaire/1 formulaire complet/exercice_formulaire/view.php
@@ -0,0 +1,93 @@
+<?php
+    session_start();
+    $connected = isset($_SESSION['connected']) && $_SESSION['connected'];
+    require_once('header.php');
+?>
+
+<?php
+    // Nombre d'éléments affichable par page
+    $nb_element = 3;
+
+    // Le numéro de la page que nous souhaitons visualiser, 1 par défaut
+    $page = getInt('page', 1);
+    // Calcul de l'offset pour la requête SQL
+    $offset = ($page - 1) * $nb_element;   // Si on est à la page 1, (1-1)*10 = OFFSET 0, si on est à la page 2, (2-1)*10 = OFFSET 10, etc.
+
+    // Filtre de recherche sur les colonnes nom/prénom/email/téléphone
+    $search = getValue('s');
+    
+    // Conditions du filtre, l'ajout des conditions se fera progressivement selon les paramètres passés
+    $conds = [];
+
+    if ($search != '')
+        $conds[] = "(firstname LIKE '%$search%' OR lastname LIKE '%$search%' OR email LIKE '%$search%' OR phone LIKE '%$search%')";
+    
+    // Souscription au mail ?
+    $mail_subscribe = getValue('mail_subscribe');
+    if ($mail_subscribe == '1')
+        $conds[] = 'mail_subscribe = 1';
+    // Souscription au téléphone ?
+    $phone_subscribe = getValue('phone_subscribe');
+    if ($phone_subscribe == '1')
+        $conds[] = 'phone_subscribe = 1';
+
+    // Construction de la clause SQL WHERE selon les conditions
+    $where = '';
+    if (count($conds) > 0)
+        $where = 'WHERE ' . implode(' AND ', $conds);
+
+    // Affichage du formulaire de recherche
+    echo '
+        <form method="GET" action="#">
+            <input type="search" placeholder="recherche" name="s" value="' . $search . '">
+            <label><input type="checkbox" name="mail_subscribe" value="1" ' . ($mail_subscribe == '1' ? 'checked' : '') . '> Contact par email</label>
+            <label><input type="checkbox" name="phone_subscribe" value="1" ' . ($phone_subscribe == '1' ? 'checked' : '') . '> Contact par téléphone</label>
+            <input type="submit" value="Rechercher">
+        </form>';
+    
+    // Ouverture de la connexion BDD (base de donnée)
+    $db = connexion();
+
+    $query = $db->query("SELECT COUNT(*) AS nb FROM utilisateur $where");
+    $nb_elements = $query->fetch()['nb'];
+    if ($nb_elements == 0)
+        die ("Aucune donnée à afficher");
+    $max_page = ceil($nb_elements / $nb_element);
+
+    $query_string = "
+        SELECT id,
+               firstname as Prénom, 
+               lastname as Nom,
+               email as Email,
+               gender as genre,
+               DATE_FORMAT(birthdate, '%d/%m/%Y') as \"Date de naissance\",
+               phone as Téléphone,
+               mail_subscribe as \"Contact par email\",
+               phone_subscribe as \"Contact par téléphone\"
+        FROM utilisateur
+        $where
+        LIMIT $nb_element OFFSET $offset
+    ";
+
+    $query = $db->query($query_string);
+
+    $results = $query->fetchAll();
+
+    $tableau = table_html($results, $connected, 'index.php');
+
+    echo $tableau;
+
+    if ($page > 1 ) // Seulement si on est sur la page 2 ou plus, afficher un bouton "Précédent"
+    {
+        echo '<a href="?' . getImplode('page', $page - 1) . '">Précédent </a>';
+    }
+    if ($page < $max_page) // Seulement si on est pas sur la dernière page, afficher un bouton "Suivant"
+    {
+        echo '<a href="?' . getImplode('page', $page + 1) . '">Suivant</a>';
+    }
+    
+?>
+
+<?php
+    require_once('footer.php');
+?>
---
+++ formulaire/1 formulaire complet/lib/fonctions.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * table_html permet de transformer un tableau PHP en table HTML
+ * @param array $array le tableau PHP a transformer
+ * @param boolean $updateable Ajoute un lien d'édition si vrai
+ * @param string $update_url Lien d'édition pour la ligne
+ * @return string table HTML
+ */
+function table_html($array, $updateable = false, $update_url = '') {
+    $table = '<table class="table"><tr>';
+    if ($updateable)
+        $table .= '<th></th>';
+    foreach(array_keys($array[0]) as $key) {
+        $table .= '<th>' . $key . '</th>';
+    }
+    $table .= '</tr>';
+    foreach($array as $row) {
+        $table .= '<tr>';
+        if ($updateable)
+            $table .= '<td class="edit"><a href="' . $update_url . '?id=' . $row['id'] .'"><img src="img/edit.png"/></a></td>';
+        foreach($row as $value) {
+            $table .= '<td>' . $value . '</td>';
+        }
+        $table .= '</tr>';
+    }
+    $table .= '</table>';
+    
+    return $table;
+}
+
+/**
+ * Redirige le navigateur sur la page $location
+ * @param string $location URL de redirection
+ */
+function redirect($location) {
+    header('Location: ' . $location);
+    die();
+}
+
+/**
+ * Récupère la valeur dans $_GET
+ * @param string $value La clé dans le tableau $_GET
+ * @param string $default La valeur par défaut si $_GET['$value'] est vide
+ * @return string La valeur du champ $_GET ou la valeur par défaut 
+ */
+function getValue($value, $default = '') {
+    return !empty($_GET[$value]) ? $_GET[$value] : $default;
+}
+
+/**
+ * Récupère la valeur transformer en entier dans $_GET
+ * @param string $value La clé dans le tableau $_GET
+ * @param string $default La valeur par défaut si $_GET['$value'] est vide
+ * @return string La valeur du champ $_GET ou la valeur par défaut 
+ */
+function getInt($value, $default = '') {
+    return (int)getValue($value, $default);
+}
+
+/**
+ * Récupère la valeur dans $_POST
+ * @param string $value La clé dans le tableau $_POST
+ * @param string $default La valeur par défaut si $_POST['$value'] est vide
+ * @return string La valeur du champ $_POST ou la valeur par défaut 
+ */
+function postValue($value, $default = '') {
+    return !empty($_POST[$value]) ? $_POST[$value] : $default;
+}
+
+/**
+ * Récupère la valeur transformer en entier dans $_POST
+ * @param string $value La clé dans le tableau $_POST
+ * @param string $default La valeur par défaut si $_POST['$value'] est vide
+ * @return string La valeur du champ $_POST ou la valeur par défaut 
+ */
+function postInt($value, $default = '') {
+    return (int)postValue($value, $default);
+}
+
+/**
+ * getImplode génère la chaine de $_GET à passer en url
+ */
+function getImplode($key = '', $new_val = '') {
+    if ($key != '')
+        $_GET[$key] = $new_val;
+    return implode('&', array_map(
+        function ($value, $key) {
+            return $key . '=' . $value;
+        }, $_GET, array_keys($_GET)));
+}
